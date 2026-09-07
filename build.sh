c4ce2930359ed244801ccad49761a613152ac2f2
sqrew <nicholaspmanson@gmail.com>
2026-09-07 14:40:05 -0400

    made framerate more stable

--- build.sh
+++ build.sh
@@ -31,10 +31,10 @@ fi
 source "$EMSDK_DIR/emsdk_env.sh"
 
 echo "Compiling src/main.c to src/main.o..."
-emcc -O3 -c src/main.c -I"$CARP_FORK_DIR/core/" --use-port=emdawnwebgpu -sUSE_GLFW=3 -sASYNCIFY=1 -sALLOW_MEMORY_GROWTH=1 -o src/main.o
+emcc -O3 -c src/main.c -I"$CARP_FORK_DIR/core/" --use-port=emdawnwebgpu -sUSE_GLFW=3 -sASYNCIFY=1  -sALLOW_MEMORY_GROWTH=1 -o src/main.o
 
 echo "Linking src/main.o to index.js..."
-em++ -O3 src/main.o --use-port=emdawnwebgpu -sUSE_GLFW=3 -sASYNCIFY=1 -sALLOW_MEMORY_GROWTH=1 -o index.js
+em++ -O3 src/main.o --use-port=emdawnwebgpu -sUSE_GLFW=3 -sASYNCIFY=1  -sALLOW_MEMORY_GROWTH=1 -o index.js
 
 echo "=== Build Succeeded! ==="
 echo "To run the local web server, run: python3 -m http.server 8000"
